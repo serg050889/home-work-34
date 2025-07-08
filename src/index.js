99d563fbd69cba415afb971691e7cce7ac2741f8
Serg <vaschenko999@gmail.com>
2025-07-08 21:16:42 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,11 +1,9 @@
 import './scss/style.scss';
-
-import logo from './assets/img/logo.jpg';
+import logo from './assets/images/logo.png';
 
 const img = document.createElement('img');
 img.src = logo;
+img.alt = 'Logo';
 document.body.appendChild(img);
-
-
 
 console.log('Проєкт працює!');
